636bc14807b70ac30265461dce2c5a931c3f8775
Whaeedil <baoppyhossen1234@gmail.com>
2024-07-13 04:31:57 +0600

    admin

--- index.js
+++ index.js
@@ -106,19 +106,24 @@ async function run() {
       res.send(result);
     });
     // admin api
-    app.get("/users/admin/:email", verifyToken, async (req, res) => {
-      const email = req.params.email;
-      if (email !== req.decoded?.email) {
-        return res.status(403).send({ massage: "forbidden access" });
+    app.get(
+      "/users/admin/:email",
+      verifyToken,
+      verifyAdmin,
+      async (req, res) => {
+        const email = req.params.email;
+        if (email !== req.decoded?.email) {
+          return res.status(403).send({ massage: "forbidden access" });
+        }
+        const query = { email: email };
+        const user = await usersCollection.findOne(query);
+        let admin = false;
+        if (user) {
+          admin = user?.role === "admin";
+        }
+        res.send({ admin });
       }
-      const query = { email: email };
-      const user = await usersCollection.findOne(query);
-      let admin = false;
-      if (user) {
-        admin = user?.role === "admin";
-      }
-      res.send({ admin });
-    });
+    );
 
     // Users Patch
     app.patch("/users/admin/:id", async (req, res) => {
